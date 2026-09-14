797290e80fdaa679d3d92c81a7a3b108d7b22727
joao-quadros <joaoquadros@ufop.edu.br>
2026-09-14 17:42:06 -0300

    Fix: adicionado import threading no topo do scipubs_flet_app.py para resolver NameError

--- scipubs_flet_app.py
+++ scipubs_flet_app.py
@@ -5,6 +5,7 @@ import os
 import time
 import pickle
 import functools
+import threading
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
